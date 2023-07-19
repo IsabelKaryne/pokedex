1b0dc2bf974d5824280d66474128410a9ca1caab
IsabelKaryne <55243963+IsabelKaryne@users.noreply.github.com>
2023-07-19 08:42:47 -0300

    Update index.js
    retirando códigos de versões anteriores

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,36 +1,3 @@
-
-//processamento assincrono. Executado e que não tem resposta de imediato. Uma hora vai ter a resposta
-//promise é a promessa de um resultado.
-//then -> faça alguma coisa
-//interface -> promessa de alguma coisa e quando for disponibilizado, vai chamar o then. caso contrário, catch. e o finally.
-// Por padrão o fetch utiliza o get sempre
-
-/*
-Uma forma de se fazer:
-
-fetch(url)
-   .then((response) => {
-       return response.json();
-   })
-   .then ((jsonBody)=>{
-       console.log(jsonBody);
-   })
-   .catch((error) => {
-       // O catch serve para caso dê erro a requisição
-       console.log(error);
-   })
-   .finally(() => {
-       //Vai ser feito independente do fracasso ou do sucesso quando finalizar a requisição, processo.
-       console.log('requisição concluída');
-   })
- //const listItems = [];
-
-   // for (let i = 0; i < pokemons.length; i++) {
-   //     const pokemon = pokemons[i];
-   //     listItems.push(convertPokemonToLi(pokemon));
-   // }
-   // console.log(listItems);
-*/
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 const popup = document.getElementById('popup');
@@ -67,13 +34,3 @@ loadMoreButton.addEventListener('click', () => {
     offset += limit;
     loadPokemonItens(offset, limit)
 })
-
-/*
-Essa linha de código acima é a mesma coisa dessa de baixo:
-
-const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon));
-
-const newHtml = newList.join();
-
-pokemonList.innerHTML += newHtml;*/
-
